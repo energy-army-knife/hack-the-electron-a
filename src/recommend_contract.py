c355ab5d0af3699f3929771cc82ac2b80d91cee9
Bruno Miguel Leonardo Galhardo <bgalhardo@gmail.com>
2019-06-20 11:05:20 +0100

    contracted power optimizer takes zeros into account

--- src/recommend_contract.py
+++ src/recommend_contract.py
@@ -14,7 +14,7 @@ def recommend_contract(meter_load, percentile=100.0):
     - 100 (max) conservative approach
     - 99 corresponds to the load value in the 99 percentile"""
 
-    load_value = np.nanpercentile(meter_load, percentile)
+    load_value = np.nanpercentile(meter_load[meter_load != 0], percentile)
 
     for contract in contracts_available:
         if load_value < contract:
@@ -46,17 +46,17 @@ if __name__ == '__main__':
     df_meter_stats[99.99] = meter_power.apply(lambda x: np.nanpercentile(x, 99.99))
     df_meter_stats[99.999] = meter_power.apply(lambda x: np.nanpercentile(x, 99.999))
 
-    plot_hist(meter_contract, filename='contract_distribution', xlabel='contracts [kW]')
-
-    chosen_per = 99.9
-    plot_bar(df_meter_stats.index, [df_meter_stats.max_value/df_meter_stats.ctrct_pw,
-                                    df_meter_stats[chosen_per]/df_meter_stats.ctrct_pw,
-                                    df_meter_stats.mean_value/df_meter_stats.ctrct_pw],
-             threshold=1,
-             filename='contracted_normalise', xlabel='load / contracted power',
-             legend_name=['max value normalised',
-                          '{} percentile normalised'.format(chosen_per),
-                          'mean value normalised'])
+    # plot_hist(meter_contract, filename='contract_distribution', xlabel='contracts [kW]')
+    #
+    # chosen_per = 99.9
+    # plot_bar(df_meter_stats.index, [df_meter_stats.max_value/df_meter_stats.ctrct_pw,
+    #                                 df_meter_stats[chosen_per]/df_meter_stats.ctrct_pw,
+    #                                 df_meter_stats.mean_value/df_meter_stats.ctrct_pw],
+    #          threshold=1,
+    #          filename='contracted_normalise', xlabel='load / contracted power',
+    #          legend_name=['max value normalised',
+    #                       '{} percentile normalised'.format(chosen_per),
+    #                       'mean value normalised'])
 
     for per in [99, 99.9, 99.99, 99.999, 100]:
         df_meter_stats['recommendation'] = meter_power.apply(lambda x: recommend_contract(x, per) * kW)
